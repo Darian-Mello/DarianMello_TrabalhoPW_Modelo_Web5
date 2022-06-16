95353b4f6c9df776d2c2a7da503de46734415614
Darian <darian.mello25@gmail.com>
2022-06-16 11:22:31 -0300

    Adicionada funcionalidade de filtro, ordenação e paginação nas listagens

--- src/main/java/br/edu/ifsul/dao/AeroportoDAO.java
+++ src/main/java/br/edu/ifsul/dao/AeroportoDAO.java
@@ -4,6 +4,7 @@
  */
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo.Aeroporto;
 import java.io.Serializable;
 import javax.ejb.Stateful;
@@ -17,5 +18,13 @@ public class AeroportoDAO<TIPO> extends DAOGenerico<Aeroporto> implements Serial
     public AeroportoDAO() {
         super();
         classesPersistence = Aeroporto.class;
+        
+        listaOrdem.add(new Ordem("id", "ID", "="));
+        listaOrdem.add(new Ordem("nome", "Nome", "like"));
+        
+        ordemAtual = listaOrdem.get(1);
+        
+        converterOrdem = new ConverterOrdem();
+        converterOrdem.setListaOrdem(listaOrdem);
     }
 }
--- src/main/java/br/edu/ifsul/dao/ClasseDAO.java
+++ src/main/java/br/edu/ifsul/dao/ClasseDAO.java
@@ -4,6 +4,7 @@
  */
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo.Classe;
 import java.io.Serializable;
 import javax.ejb.Stateful;
@@ -17,5 +18,13 @@ public class ClasseDAO<TIPO> extends DAOGenerico<Classe> implements Serializable
     public ClasseDAO() {
         super();
         classesPersistence = Classe.class;
+                
+        listaOrdem.add(new Ordem("id", "ID", "="));
+        listaOrdem.add(new Ordem("nome", "Nome", "like"));
+        
+        ordemAtual = listaOrdem.get(1);
+        
+        converterOrdem = new ConverterOrdem();
+        converterOrdem.setListaOrdem(listaOrdem);
     }
 }
